47be48d5e47467114487f6fbd02dbbcd10792446
Ethan Zhang <ethan.zhang@ringcentral.com>
2020-06-03 13:04:29 +0800

    chore(*) Update typescript

---
+++ compile.d.ts
@@ -0,0 +1,2 @@
+import * as ts from 'typescript';
+export default function compile(filePaths: string[], writeFileCallback?: ts.WriteFileCallback): void;
---
+++ transformer.d.ts
@@ -0,0 +1,3 @@
+import * as ts from 'typescript';
+declare const _default: (program: ts.Program) => ts.TransformerFactory<ts.SourceFile>;
+export default _default;
